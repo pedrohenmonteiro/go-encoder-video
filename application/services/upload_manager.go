5cb11a928b0f99234ad922c5bf9a5114682dec40
Pedro Monteiro <pedromont436@gmail.com>
2024-01-30 00:44:09 -0300

    criando funçoes privadas getClientUpload e loadPaths

--- application/services/upload_manager.go
+++ application/services/upload_manager.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -43,4 +44,30 @@ func (vu *VideoUpload) UploadObject(objectPath string, client *storage.Client, c
 	}
 	return nil
 
+}
+
+func (vu *VideoUpload) loadPaths() error {
+	err := filepath.Walk(vu.VideoPath, func(path string, info os.FileInfo, err error) error {
+
+		if !info.IsDir() {
+			vu.Paths = append(vu.Paths, path)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (vu *VideoUpload) getClientUpload() (*storage.Client, context.Context, error) {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	return client, ctx, nil
 }
