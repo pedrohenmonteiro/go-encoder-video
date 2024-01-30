7117f0f5dffdb7d3bd98355d8582bce023fe3e11
Pedro Monteiro <pedromont436@gmail.com>
2024-01-29 23:48:02 -0300

    criando e testando encode de video

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -79,6 +79,26 @@ func (v *VideoService) Fragment() error {
 	return nil
 }
 
+func (v *VideoService) Encode() error {
+	cmdArgs := []string{}
+	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID+".frag")
+	cmdArgs = append(cmdArgs, "--use-segment-timeline")
+	cmdArgs = append(cmdArgs, "-o")
+	cmdArgs = append(cmdArgs, os.Getenv("localStoragePath")+"/"+v.Video.ID)
+	cmdArgs = append(cmdArgs, "-f")
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/usr/local/bin")
+	cmd := exec.Command("mp4dash", cmdArgs...)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	printOutput(output)
+
+	return nil
+}
+
 func printOutput(out []byte) {
 	if len(out) > 0 {
 		log.Printf("=====> Output: %s\n", string(out))
